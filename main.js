5fc7b7250aabed57cb2f891549b16f5090d90918
Luka <luka.gouvea0@gmail.com>
2024-02-06 20:16:56 -0300

    feat: add genetic algorithm

--- main.js
+++ main.js
@@ -11,13 +11,34 @@ const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
 
 const N = 100;
 const cars = generateCars(N);
+let bestCar = cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i = 0; i < cars.length; i++){
+        cars[i].brain = JSON.parse(
+            localStorage.getItem("bestBrain")
+        );
+        if (i != 0){
+            NeuralNetwork.mutate(cars[i].brain, 0.2);
+        }
+    }
+}
 
 
 const traffic = [
     new Car(road.getLaneCenter(1), -100,30,50, "DUMMY", 2),
+    new Car(road.getLaneCenter(0), -300,30,50, "DUMMY", 2),
+    new Car(road.getLaneCenter(2), -300,30,50, "DUMMY", 2),
 ]
 
 animate();
+
+function save() {
+    localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+    localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N){
     const cars = [];
@@ -35,13 +56,19 @@ function animate(time){
     for(let i = 0; i < cars.length; i++){
         cars[i].update(road.borders, traffic);
     }
+
+    bestCar = cars.find(
+        c => c.y === Math.min(
+            ...cars.map(c=> c.y)
+        )
+    );
     
 
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
 
     carCtx.save();
-    carCtx.translate(0,-cars[0].y + carCanvas.height*0.7);
+    carCtx.translate(0,-bestCar.y + carCanvas.height*0.7);
 
     road.draw(carCtx);
     for(let i = 0; i < traffic.length; i++){
@@ -53,13 +80,13 @@ function animate(time){
         cars[i].draw(carCtx, "blue");
     }
     carCtx.globalAlpha = 1
-    cars[0].draw(carCtx, "blue", true);
+    bestCar.draw(carCtx, "blue", true);
 
 
 
     carCtx.restore();
 
     networkCtx.lineDashOffset = -time/ 50;
-    Visualizer.drawNetwork(networkCtx, cars[0].brain);
+    Visualizer.drawNetwork(networkCtx, bestCar.brain);
     requestAnimationFrame(animate);
 }
