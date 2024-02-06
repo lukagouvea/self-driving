f826c3ec808b84027be85f296e303fc4106ae93b
Luka <luka.gouvea0@gmail.com>
2024-02-06 02:19:56 -0300

    feat: add neural network

--- main.js
+++ main.js
@@ -1,20 +1,65 @@
-const canvas = document.getElementById("myCanvas");
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
 
-canvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 500;
 
-const ctx = canvas.getContext("2d");
-const road = new Road(canvas.width/2,canvas.width*0.9);
-const car = new Car(100,100,30,50);
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
 
-car.draw(ctx);
+const road = new Road(carCanvas.width/2,carCanvas.width*0.9);
+
+const N = 100;
+const cars = generateCars(N);
+
+
+const traffic = [
+    new Car(road.getLaneCenter(1), -100,30,50, "DUMMY", 2),
+]
 
 animate();
 
-function animate(){
-    car.update();
+function generateCars(N){
+    const cars = [];
+    for(let i = 1; i <= N; i++){
+        cars.push(new Car(road.getLaneCenter(Math.random() % road.laneCount), 100, 30, 50, "AI"));
+    }
+    return cars;
+}
 
-    canvas.height = window.innerHeight;
-    road.draw(ctx);
-    car.draw(ctx);
+function animate(time){
+    for(let i = 0; i < traffic.length; i++){
+        traffic[i].update(road.borders, []);
+    }
+    
+    for(let i = 0; i < cars.length; i++){
+        cars[i].update(road.borders, traffic);
+    }
+    
+
+    carCanvas.height = window.innerHeight;
+    networkCanvas.height = window.innerHeight;
+
+    carCtx.save();
+    carCtx.translate(0,-cars[0].y + carCanvas.height*0.7);
+
+    road.draw(carCtx);
+    for(let i = 0; i < traffic.length; i++){
+        traffic[i].draw(carCtx, "red");
+    }
+
+    carCtx.globalAlpha = 0.2
+    for(let i = 0; i < cars.length; i++){
+        cars[i].draw(carCtx, "blue");
+    }
+    carCtx.globalAlpha = 1
+    cars[0].draw(carCtx, "blue", true);
+
+
+
+    carCtx.restore();
+
+    networkCtx.lineDashOffset = -time/ 50;
+    Visualizer.drawNetwork(networkCtx, cars[0].brain);
     requestAnimationFrame(animate);
 }
